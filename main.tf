f89c92b6f16bd239378ac129bdb660667d59ab23
Gatsby10 <abdullahidamola@gmail.com>
2026-06-16 11:53:18 +0100

    creating pub,pri subnet and security groups

--- main.tf
+++ main.tf
@@ -124,4 +124,290 @@ resource "aws_route_table_association" "prisub-2-association" {
     route_table_id = aws_route_table.private-route-table.id
 }
 
+# Keypair created for SSH into instance
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "key" {
+  content         = tls_private_key.key.private_key_pem
+  filename        = "keypair.pem"
+  file_permission = "0400"
+}
+
+resource "aws_key_pair" "key" {
+  key_name   = "keypair"
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+# Security group created for HTTP, HTTPS and SSH access
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow HTTP, HTTPS and SSH traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = var.https_port
+    to_port     = var.https_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow Jenkins"
+    from_port   = var.jenkins_port
+    to_port     = var.jenkins_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  tags = {
+    Name = "${local.name}-jenkins-sg"
+  }
+}
+
+# Security group created for SonarQube access
+
+resource "aws_security_group" "sonarqube_sg" {
+  name        = "sonarqube-sg"
+  description = "Allow HTTP, HTTPS, SSH and SonarQube traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow SSH - Port 22"
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow HTTP - Port 80"
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow HTTPS - Port 443"
+    from_port   = var.https_port
+    to_port     = var.https_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow SonarQube - Port 9000"
+    from_port   = var.sonar_port
+    to_port     = var.sonar_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  tags = {
+    Name = "${local.name}-sonarqube-sg"
+  }
+}
+
+# Security group created for Ansible access
+resource "aws_security_group" "ansible_sg" {
+  name        = "ansible-sg"
+  description = "Allow SSH traffic for Ansible"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow SSH - Port 22"
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  tags = {
+    Name = "${local.name}-ansible-sg"
+  }
+}
+
+# Security group created for Docker access
+resource "aws_security_group" "docker_sg" {
+  name        = "docker-sg"
+  description = "Allow HTTP, HTTPS, SSH and Docker traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow SSH - Port 22"
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow HTTP - Port 80"
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow HTTPS - Port 443"
+    from_port   = var.https_port
+    to_port     = var.https_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow Docker - Port 2375"
+    from_port   = var.docker_port
+    to_port     = var.docker_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow Docker TLS - Port 2376"
+    from_port   = var.dockertls_port
+    to_port     = var.dockertls_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  tags = {
+    Name = "${local.name}-docker-sg"
+  }
+}
+
+# Security group created for Nexus access
+resource "aws_security_group" "nexus_sg" {
+  name        = "nexus-sg"
+  description = "Allow HTTP, HTTPS, SSH and Nexus traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow SSH - Port 22"
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow HTTP - Port 80"
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow HTTPS - Port 443"
+    from_port   = var.https_port
+    to_port     = var.https_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  ingress {
+    description = "Allow Nexus - Port 8081"
+    from_port   = var.nexus_port
+    to_port     = var.nexus_port
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  tags = {
+    Name = "${local.name}-nexus-sg"
+  }
+}
+
+# Security group created for RDS access
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow RDS database traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow MySQL RDS - Port 3306"
+    from_port   = var.mysql_port
+    to_port     = var.mysql_port
+    protocol    = "tcp"
+    cidr_blocks = [var.rds_cidr]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.all_cidr_blocks]
+  }
+
+  tags = {
+    Name = "${local.name}-rds-sg"
+  }
+}
+
 
